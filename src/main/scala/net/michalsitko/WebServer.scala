483e0d9ed86ffbba79e5f2c00e89db8765c4e929
Michał Sitko <pierwszy1@gmail.com>
2016-11-16 10:26:15 +0100

    version endpoint formatted with circe

--- src/main/scala/net/michalsitko/WebServer.scala
+++ src/main/scala/net/michalsitko/WebServer.scala
@@ -2,24 +2,33 @@ package net.michalsitko
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import de.heikoseeberger.akkahttpcirce.CirceSupport
+import io.circe.{Encoder, Json}
+import io.circe.syntax._
 
-object WebServer {
+object WebServer extends AnyRef with CirceSupport {
   def main(args: Array[String]) {
 
-    implicit val system = ActorSystem("my-system")
+    implicit val system = ActorSystem("crud-template-http-system")
     implicit val materializer = ActorMaterializer()
 
     val route =
       path("version") {
         get {
-          val buildInfo = net.michalsitko.BuildInfo.toString
-          complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, buildInfo))
+          complete(BuildInfo)
         }
       }
 
     Http().bindAndHandle(route, "localhost", 8080)
+  }
+
+  private implicit val encodeBuildInfo: Encoder[BuildInfo.type] = new Encoder[BuildInfo.type] {
+    final def apply(buildInfo: BuildInfo.type): Json = {
+      val fieldsMap = Map("version" -> buildInfo.version, "scalaVersion" -> buildInfo.scalaVersion) ++
+                buildInfo.gitHeadCommit.fold(Map.empty[String, String])(c => Map("commit" -> c))
+      fieldsMap.asJson
+    }
   }
 }
