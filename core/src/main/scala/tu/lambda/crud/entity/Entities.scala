c2a2b1a1699edc3ac2751a9f6fbe9124ae9d653b
Michał Sitko <pierwszy1@gmail.com>
2018-07-01 13:17:49 +0200

    wip

--- core/src/main/scala/tu/lambda/crud/entity/Entities.scala
+++ core/src/main/scala/tu/lambda/crud/entity/Entities.scala
@@ -2,8 +2,29 @@ package tu.lambda.crud.entity
 
 import java.net.URL
 
-// In real world project storing password as String might not be good idea
 final case class User(email: String, phone: String, password: String)
+/*
+Naively modelled:
+case class User(id: UUID, email: String, phone: String, password: String)
+
+Now when creating a new user:
+
+User(null, email, phone, password)
+
+That's quite terrible as it uses null. Quoting Tony Hoare:
+
+I call it my billion-dollar mistake. It was the invention of the null reference in 1965.
+At that time, I was designing the first comprehensive type system for references
+in an object oriented language (ALGOL W).
+This has led to innumerable errors, vulnerabilities, and system crashes, which have
+probably caused a billion dollars of pain and damage in the last forty years.
+
+A bit better:
+
+case class User(id: Option[UUID], email: String, phone: String, password: String)
+
+But the essence of the problem remains - we are modelling two different entities with the same class.
+ */
 
 final case class SavedUser(id: UserId, email: String, phone: String)
 
@@ -12,7 +33,6 @@ object SavedUser {
     SavedUser(id, user.email, user.phone)
 }
 
-// TalkExample: customer circe Decoder
 final case class BookmarksImport(bookmarks: List[Bookmark])
 
 final case class Bookmark(url: URL, description: String)
