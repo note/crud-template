26077d1754b231c5a59484268eccc0e8ca4074d8
Michał Sitko <pierwszy1@gmail.com>
2018-07-01 21:32:37 +0200

    more in amm_predef

--- amm_predef.scala
+++ amm_predef.scala
@@ -5,4 +5,9 @@ import cats.effect._
 import cats.implicits._
 import io.circe._, io.circe.generic.semiauto._, io.circe.parser._, io.circe.syntax._
 
+import tu.lambda.crud.entity._
+
+val user = User("email@email.com", "123000321", "pass")
+val savedUser = SavedUser.fromUser(UserId(java.util.UUID.randomUUID), user)
+
 println("Loaded custom amm_predef")
